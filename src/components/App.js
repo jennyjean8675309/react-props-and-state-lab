e203752349b3b72762a6490501e12670504356e1
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2020-03-30 12:42:45 -0400

    completed solution branch for pet adoption lab

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,61 @@ class App extends React.Component {
     }
   }
 
+  // remember that a callback prop is ALWAYS a function - the function has to be declared here, inside of App, because it needs access to App's this.state in order to update state
+
+  // in React we use arrow functions to implicitly bind the value of 'this' - if you run into an error where 'this' is not defined, it most likely means that you're not using an arrow function
+
+  onChangeType = (event) => {
+    console.log('changing the filter type...')
+
+    // this.state.filters.type = event.target.value
+    // console.log('changing state...', this.state.filters.type)
+
+    // you CAN just change state directly like this - HOWEVER, when you change state this way, React doesn't recognize the change and WILL NOT TRIGGER A RE-RENDER
+
+    // you MUST use the setState() function to update state
+
+    this.setState({
+      filters: { type: event.target.value }
+    })
+  }
+
+  onFindPetsClick = () => {
+    console.log('on Find Pets was clicked...')
+
+    let url = '/api/pets'
+
+    if (this.state.filters.type !== 'all') {
+      url = url + `?type=${this.state.filters.type}`
+    }
+    
+    fetch(url)
+    .then(resp => resp.json())
+    .then(petData => this.setState({
+      pets: petData
+    }))
+    .catch(error => {
+      alert(`Error: ${error}`)
+    })
+  }
+
+  onAdoptPet = (petId) => {
+    console.log('adopting a pet...', petId)
+
+    let updatedPets = this.state.pets.map(pet => {
+      if (pet.id === petId) {
+        // we can use the spread operator here to make our code more efficient
+        return {...pet, isAdopted: true}
+      } else {
+        return pet
+      }
+    })
+
+    this.setState({
+      pets: updatedPets
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +79,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,35 @@
 import React from 'react'
 
 class Pet extends React.Component {
+
+  // here, I've used destructuring to separate out the keys of my pet object to use as variables - this keeps my code more DRY, as otherwise, I would have to type out this.props.pet.gender, this.props.pet.name, etc., each time that I wanted to access a value
+  
   render() {
+    let {id, name, type, age, weight, gender, isAdopted} = this.props.pet
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {gender === 'male' ? '♂ ' : '♀ '}
+            {/* { remember, in JSX we have to use ternary statements in order to conditionally render things - an ordinary if/else statement would not work } */}
+            {name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {age}</p>
+            <p>Weight: {weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {/* {if you need to write a ternary that is more than one line long, use () to add multiple lines and keep your code more clean} */}
+          { isAdopted ? (
+            <button className="ui disabled button">Already adopted</button>
+          ) : (
+            <button className="ui primary button" onClick={() => this.props.onAdoptPet(id)}>Adopt pet</button>
+            // { just as with vanilla JS, we can pass information to a callback function by refactoring our code so that the callback is an anonymous arrow function in which we invoke the function inside with an argument }
+          ) } 
         </div>
       </div>
     )
